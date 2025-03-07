31df7a1cda1d89a6ec630701fa4fffe372341bd7
Tea713 <thanhnguyen_2025@depauw.edu>
2025-03-07 00:16:32 -0500

    refactor game logic to use tokens for player moves and enhance board management

--- main.js
+++ main.js
@@ -1,73 +1,92 @@
-function Player(name, move) {
+function Player(name, token) {
     this.name = name;
-    this.move = move;
+    this.token = token;
 }
 
-const theGame = (function () {
+const theGameBoard = (function () {
     let board = Array(3)
         .fill(null)
         .map(() => Array(3).fill(0));
-    let turns = 0;
-    let player1 = new Player("Player 1", (x, y) => {
-        board[x][y] = 1;
-    });
-    let player2 = new Player("Player 2", (x, y) => {
-        board[x][y] = 2;
-    });
 
-    const p1Move = (x, y) => player1.move(x, y);
-    const p2Move = (x, y) => player2.move(x, y);
     const clearBoard = () => {
         board = Array(3)
             .fill(null)
-            .map(() => Array(3).fill(0));
-        turns = 0;
+            .map(() => Array(3).fill(-1));
     };
+
+    const playerMove = (token, x, y) => {
+        board[x][y] = token;
+    };
+
     const checkBoard = () => {
         for (let i = 0; i < 3; i++) {
-            if (board[i][0] !== 0 && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-                declareWinner(board[i][0]);
+            if (
+                board[i][0] !== 0 &&
+                board[i][0] === board[i][1] &&
+                board[i][1] === board[i][2]
+            ) {
                 return true;
             }
         }
 
         for (let i = 0; i < 3; i++) {
-            if (board[0][i] !== 0 && board[0][i] === board[1][i] && board[i][1] === board[i][2]) {
-                declareWinner(board[i][0]);
+            if (
+                board[0][i] !== 0 &&
+                board[0][i] === board[1][i] &&
+                board[i][1] === board[i][2]
+            ) {
                 return true;
             }
         }
-        if (board[0][0] !== 0 && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-            declareWinner(board[0][0]);
+        if (
+            board[0][0] !== 0 &&
+            board[0][0] === board[1][1] &&
+            board[1][1] === board[2][2]
+        ) {
             return true;
         }
-        if (board[0][2] !== 0 && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-            declareWinner(board[0][2]);
+        if (
+            board[0][2] !== 0 &&
+            board[0][2] === board[1][1] &&
+            board[1][1] === board[2][0]
+        ) {
             return true;
         }
         return false;
     };
 
-    const declareWinner = (player) => {
-        if (player === 1) {
-            console.log(`${player1.name} has won!`);
-        } else {
-            console.log(`${player2.name} has won!`);
-        }
-    };
-    const printBoard = () => console.log(board);
+    const getBoard = () => board;
 
-    return { printBoard, p1Move, p2Move, clearBoard, checkBoard };
+    return { getBoard, playerMove, clearBoard, checkBoard };
 })();
 
-theGame.p1Move(0, 0);
-theGame.checkBoard();
-theGame.p2Move(1, 1);
-theGame.checkBoard();
-theGame.p1Move(0, 1);
-theGame.checkBoard();
-theGame.p2Move(1, 2);
-theGame.checkBoard();
-theGame.p1Move(0, 2);
-theGame.checkBoard();
-theGame.printBoard();
+const gameController = (function () {
+    let player1 = new Player("Player 1", 1);
+    let player2 = new Player("Player 2", 2);
+    let activePlayer = player1;
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    };
+
+    const playTurn = (x, y) => {
+        theGameBoard.playerMove(activePlayer.token, x, y);
+        console.log(theGameBoard.getBoard());
+        if (theGameBoard.checkBoard()) {
+            console.log(`${activePlayer.name} has won!`);
+            return true;
+        }
+        switchPlayer();
+        return false;
+    };
+
+    return { playTurn };
+})();
+
+
+gameController.playTurn(0, 0);
+gameController.playTurn(1, 0);
+gameController.playTurn(0, 1);
+gameController.playTurn(1, 1);
+gameController.playTurn(2, 2);
+gameController.playTurn(1, 2);
